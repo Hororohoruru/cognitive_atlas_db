a346f71d0f63c8c9016fac5c828a19af1c6447f5
Juan Jesus Torre <juanjesustorre@gmail.com>
2019-07-10 13:43:06 +0200

    package structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='LabelsDb',
+      version='0.1',
+      description='A tool for managing cognitive labels files',
+      url='https://github.com/Hororohoruru/cognitive_atlas_db',
+      author='Juan Jesus Torre',
+      author_email='juanjesustorre@gmail.com',
+      packages=['core'],
+)
